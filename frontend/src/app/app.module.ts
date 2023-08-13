f25c3fdfa8c8f2b3c56484e236aedab21e69cb25
Markus Thielker <mail.markus.thielker@gmail.com>
2023-08-13 18:07:03 +0200

    TEMPLATE-2: add remote service
    This service handles the http requests. It should only be called by the 'local' service pendent.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from "./pages/home/home.component";
+import { HomeComponent } from './pages/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HomeComponent } from "./pages/home/home.component";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
